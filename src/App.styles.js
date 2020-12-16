f184bcf078f4bb0d17194008a0ad1b0baeed5bcd
Mohammed Magdy Ismael <m.magdy.isl@gmail.com>
2020-12-16 17:39:02 +0200

    Add new features

---
+++ src/App.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+export const HeaderContainer = styled.div``;
+export const Title = styled.p`
+    font-family: sans-serif;
+    font-size: 25px;
+    font-weight: 700;
+`;
+export const CodeEnteryContainer = styled.div``;
+export const SubTitle = styled(Title)`
+    font-size: 18px;
+    font-weight: 600;
+`;
+export const InputField = styled.textarea``;
+export const FeedbackEnteryContainer = styled.div``;
+export const Button = styled.button`
+    display: block;
+`;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const baseUrl  =   "https://api.openweathermap.org/data/2.5/weather?q=";
+export const metric   =   "&units=metric";
+export const apiKey   =   "&appid=76c5069562d747a58b45fc5e21041876";
